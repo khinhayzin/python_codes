fea8432fb934b8a09eb6140e0a45fe5330ff93d0
khinhayzin <aungshinthant20052007@gmail.com>
2024-04-29 10:53:00 +0100

    test12.py

---
+++ test12.py
@@ -0,0 +1,15 @@
+myfav_artists = ['Htoo eain Thin' , 'Christ' , 'Aye Chan' , 'Ko Lin' , 'Khin']
+print(type(myfav_artists))
+
+print(myfav_artists[2])
+print(myfav_artists[1:4])
+print(myfav_artists[1:])
+print(myfav_artists[:4])
+
+
+
+
+# print(myfav_artists)
+# print(myfav_artists[2])
+# print(len(myfav_artists))
+
