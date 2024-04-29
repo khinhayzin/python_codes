b1ed63eefdc6de5b3fb7fa96f7a15360a25e73b2
khinhayzin <aungshinthant20052007@gmail.com>
2024-04-29 11:30:54 +0100

    test13.py

--- test12.py
+++ test12.py
@@ -1,11 +1,21 @@
 myfav_artists = ['Htoo eain Thin' , 'Christ' , 'Aye Chan' , 'Ko Lin' , 'Khin']
-print(type(myfav_artists))
+# print(type(myfav_artists))
 
-print(myfav_artists[2])
-print(myfav_artists[1:4])
-print(myfav_artists[1:])
-print(myfav_artists[:4])
+# print(myfav_artists[2])
+# print(myfav_artists[1:4])
+# print(myfav_artists[1:])
+# print(myfav_artists[:4])
 
+# print(myfav_artists)
+
+# myfav_artists.insert(2, 'Oppa')
+# print(myfav_artists)
+
+# myfav_artists.sort()
+# print(myfav_artists)
+
+myfav_artists[0] = "Khin's fav"
+print(myfav_artists)
 
 
 
---
+++ test13.py
@@ -0,0 +1,9 @@
+artists_raymond = {
+    'name' : 'Wai Mon Oo',
+    'genre' : 'rock'
+}
+
+print(type(artists_raymond))
+print(artists_raymond['name'])
+artists_raymond["popular_song"] = "Thu Ye Gaung"
+print(artists_raymond)
