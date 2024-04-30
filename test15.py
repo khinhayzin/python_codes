f3d2592ea74664b8d1b70136bb44c7817fdb18a8
khinhayzin <aungshinthant20052007@gmail.com>
2024-04-30 11:19:25 +0100

    test14.py

---
+++ test15.py
@@ -0,0 +1,13 @@
+# def tales(story):
+#     print("I like" , story)
+
+# def add(x,y):
+#     print(x+y)
+
+# def sub(x,y):
+#     print(x-y)
+
+def info(name, mail, addr='yangon'):
+    print("Your name is ", name)
+    print("Your mail is ", mail)
+    print("Your addr is ", addr)
---
+++ test16.py
@@ -0,0 +1,9 @@
+from test15 import *
+
+# tales("Usher")
+# add(7,9)
+# sub(9,2)
+
+info("Khin", "khin@gmail.com" )
+info(mail='khin@gmail.com', addr='yangon', name='Khin')
+info(mail='khin@gmail.com', name='KTS')
